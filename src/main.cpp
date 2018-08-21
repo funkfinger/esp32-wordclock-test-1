5d7aafd9608f66e83536f68f3e42030868fa6bf3
Jay Wiggins <jay@irk.com>
2018-08-21 14:52:51 -0700

    coordinate system and leds lighting...

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,61 @@
+#define IGNORE_WIFI
+
 #include <Arduino.h>
 
-void setup() {
-    // put your setup code here, to run once:
+#if !(defined(IGNORE_WIFI))
+  #include <WiFi.h>
+#endif
+
+#define FASTLED_ALLOW_INTERRUPTS 0
+#include <FastLED.h>
+
+#define ROWS 19
+#define COLS 16
+#define NUM_LEDS ROWS*COLS
+
+#define DATA_PIN 13
+
+#define IGNORE_SERIAL
+
+CRGB leds[NUM_LEDS];
+
+#include "settings.h"
+
+// function to orient correctly...
+void light(uint8_t x, uint8_t y, uint8_t h) {
+  // uint8_t realRow = ROWS - y + 1;
+  // uint16_t pixel = realRow * COLS + (y - 1);
+  uint16_t pixel = (ROWS-y+1)*COLS-(COLS-x+1);
+  leds[pixel] = CHSV(h, 255, 255); 
 }
 
+void setup() {
+  delay(1000); // 3 second delay for recovery
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  #if !(defined(IGNORE_SERIAL))
+    Serial.begin(9600);
+  #endif
+  #if !(defined(IGNORE_WIFI))
+    WiFi.begin(SETTINGS_NETWORK_SSID, SETTINGS_NETWORK_PASS);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.println("Connecting to WiFi..");
+    }
+    Serial.println("Connected to the WiFi network");
+  #endif
+}
+
+static int hue=0;
+
 void loop() {
-    // put your main code here, to run repeatedly:
+  light(1, 1, 10);
+  light(16, 19, 40);
+  light(16, 1, 80);
+  light(1, 19, 120);
+  // light(2, 2, 60);
+  // light(2, 3, 100);
+ 
+  leds[5] = CHSV(10, 255, 255);
+  FastLED.show();
+  delay(5);
 }
