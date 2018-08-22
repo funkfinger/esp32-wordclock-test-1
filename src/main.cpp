311c869bc7a93071f0d17bb427bc7efd100fede9
Jay Wiggins <jay@irk.com>
2018-08-22 10:57:38 -0700

    adding wifi manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
-#define IGNORE_WIFI
+// #define IGNORE_WIFI
+// #define IGNORE_SERIAL
 
 #include <Arduino.h>
+#include <RTClib.h>
+#include <TimeLib.h>
 
-#if !(defined(IGNORE_WIFI))
-  #include <WiFi.h>
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>          
+#else
+#include <WiFi.h>          
 #endif
+
+//needed for library
+#include <DNSServer.h>
+#if defined(ESP8266)
+#include <ESP8266WebServer.h>
+#else
+#include <WebServer.h>
+#endif
+#include <WiFiManager.h> 
+
+
+// for ntp time gathering...
+#define TIME_OFFSET -25200
+#define UPDATE_INTERVAL 60000
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "time.google.com", TIME_OFFSET, UPDATE_INTERVAL);
 
 #define FASTLED_ALLOW_INTERRUPTS 0
 #include <FastLED.h>
@@ -15,9 +40,14 @@
 
 #define DATA_PIN 13
 
-#define IGNORE_SERIAL
+#define $IT [1, 2]
 
+
+// leds var...
 CRGB leds[NUM_LEDS];
+
+// real time clock var...
+RTC_DS3231 rtc;
 
 #include "settings.h"
 
@@ -32,17 +62,37 @@ void light(uint8_t x, uint8_t y, uint8_t h) {
 void setup() {
   delay(1000); // 3 second delay for recovery
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  #if !(defined(IGNORE_SERIAL))
-    Serial.begin(9600);
-  #endif
-  #if !(defined(IGNORE_WIFI))
-    WiFi.begin(SETTINGS_NETWORK_SSID, SETTINGS_NETWORK_PASS);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.println("Connecting to WiFi..");
-    }
-    Serial.println("Connected to the WiFi network");
-  #endif
+
+  // begin real time clock...
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  // load time from network if rtc has lost power...
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power- set time from NTP");
+    timeClient.update();
+    setTime(timeClient.getEpochTime());
+    rtc.adjust(DateTime(year(), month(), day(), hour(), minute(), second()));
+  }
+
+  // set a now var...
+  DateTime now = rtc.now();
+  setTime(now.unixtime());
+  delay(500);
+
+  Serial.begin(9600);
+  // WiFi.begin(SETTINGS_NETWORK_SSID, SETTINGS_NETWORK_PASS);
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.println("Connecting to WiFi..");
+  // }
+  // Serial.println("Connected to the WiFi network");
+
+  WiFiManager wifiManager;
+  // wifiManager.autoConnect(SETTINGS_NETWORK_SSID, SETTINGS_NETWORK_PASS);
+  wifiManager.startConfigPortal("WordClockAP");
 }
 
 static int hue=0;
@@ -58,4 +108,21 @@ void loop() {
   leds[5] = CHSV(10, 255, 255);
   FastLED.show();
   delay(5);
+
+  Serial.print(year(), DEC);
+  Serial.print('/');
+  Serial.print(month(), DEC);
+  Serial.print('/');
+  Serial.print(day(), DEC);
+  Serial.print(" (");
+  Serial.print(monthStr(month()));
+  Serial.print(") ");
+  Serial.print(hour(), DEC);
+  Serial.print(':');
+  Serial.print(minute(), DEC);
+  Serial.print(':');
+  Serial.print(second(), DEC);
+  Serial.println();
+
+  delay(5000);
 }
