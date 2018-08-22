4454ed5bb2e3c021dc94a8117805b4543b0d6b11
Jay Wiggins <jay@irk.com>
2018-08-22 16:02:22 -0700

    adding word number handling

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,14 @@
 // #define IGNORE_SERIAL
 
 #include <Arduino.h>
+#include <Wire.h>
 #include <RTClib.h>
 #include <TimeLib.h>
+
+#define I2C_SDA 21
+#define I2C_SCL 22
+// TwoWire.begin(I2C_SDA, I2C_SCL);
+TwoWire myWire(0);
 
 #include <WiFi.h>
 #include <NTPClient.h>
@@ -27,8 +33,10 @@
 
 // for ntp time gathering...
 #define TIME_OFFSET -25200
-#define UPDATE_INTERVAL 60000
+// #define UPDATE_INTERVAL 60000
+#define UPDATE_INTERVAL 60
 WiFiUDP ntpUDP;
+// NTPClient timeClient(ntpUDP);
 NTPClient timeClient(ntpUDP, "time.google.com", TIME_OFFSET, UPDATE_INTERVAL);
 
 #define FASTLED_ALLOW_INTERRUPTS 0
@@ -40,8 +48,31 @@ NTPClient timeClient(ntpUDP, "time.google.com", TIME_OFFSET, UPDATE_INTERVAL);
 
 #define DATA_PIN 13
 
-#define $IT [1, 2]
+// setup words...
+const PROGMEM int w_IT[] = {1, 1, 2};
+const PROGMEM int w_IS[] = {4, 1, 2};
 
+// hours...
+const PROGMEM int w_ONE[3]   = {6, 9, 3};
+const PROGMEM int w_TWO[3]   = {4, 9, 3};
+const PROGMEM int w_THREE[3] = {1, 12, 5};
+const PROGMEM int w_FOUR[3]  = {6, 9, 3};
+const PROGMEM int w_FIVE[3]  = {6, 9, 3};
+
+const PROGMEM int HOURS[][3] = {
+  {6, 9, 3}, // one
+  {4, 9, 3}, // two
+  {1, 12, 5}, // three
+  {7, 11, 4}, // four
+  {11, 11, 4}, // five
+  {13, 10, 3}, // six
+  {2, 19, 5}, // seven (beven)
+  {9, 10, 5}, // eight
+  {6, 10, 4}, // nine
+  {13, 10, 3}, // ten
+  {8, 9, 6}, // eleven
+  {1, 12, 6} // twelve
+};
 
 // leds var...
 CRGB leds[NUM_LEDS];
@@ -53,15 +84,27 @@ RTC_DS3231 rtc;
 
 // function to orient correctly...
 void light(uint8_t x, uint8_t y, uint8_t h) {
-  // uint8_t realRow = ROWS - y + 1;
-  // uint16_t pixel = realRow * COLS + (y - 1);
   uint16_t pixel = (ROWS-y+1)*COLS-(COLS-x+1);
   leds[pixel] = CHSV(h, 255, 255); 
 }
 
+void lightWord(const int word[], int h) {
+  for(int i = 0; i < word[2]; i++) {
+    light(word[0]+i, word[1], h);
+  }
+}
+
 void setup() {
   delay(1000); // 3 second delay for recovery
+  myWire.begin(I2C_SDA, I2C_SCL, 100000);
+  Serial.begin(9600);
+
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  
+  // begin wifi with wifimanager...
+  WiFiManager wifiManager;
+  wifiManager.autoConnect(SETTINGS_NETWORK_SSID, SETTINGS_NETWORK_PASS);
+  // wifiManager.startConfigPortal("WordClockAP");
 
   // begin real time clock...
   if (! rtc.begin()) {
@@ -73,6 +116,11 @@ void setup() {
   if (rtc.lostPower()) {
     Serial.println("RTC lost power- set time from NTP");
     timeClient.update();
+    delay(10);
+    Serial.println(timeClient.getFormattedTime());
+    timeClient.forceUpdate();
+    delay(10);
+    Serial.println(timeClient.getFormattedTime());
     setTime(timeClient.getEpochTime());
     rtc.adjust(DateTime(year(), month(), day(), hour(), minute(), second()));
   }
@@ -81,23 +129,13 @@ void setup() {
   DateTime now = rtc.now();
   setTime(now.unixtime());
   delay(500);
-
-  Serial.begin(9600);
-  // WiFi.begin(SETTINGS_NETWORK_SSID, SETTINGS_NETWORK_PASS);
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(500);
-  //   Serial.println("Connecting to WiFi..");
-  // }
-  // Serial.println("Connected to the WiFi network");
-
-  WiFiManager wifiManager;
-  // wifiManager.autoConnect(SETTINGS_NETWORK_SSID, SETTINGS_NETWORK_PASS);
-  wifiManager.startConfigPortal("WordClockAP");
 }
 
-static int hue=0;
+static int hue = 0;
+static int a = 0;
 
 void loop() {
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
   light(1, 1, 10);
   light(16, 19, 40);
   light(16, 1, 80);
@@ -105,9 +143,10 @@ void loop() {
   // light(2, 2, 60);
   // light(2, 3, 100);
  
+  lightWord(w_IT, 80);
+  lightWord(w_IS, 80);
+
   leds[5] = CHSV(10, 255, 255);
-  FastLED.show();
-  delay(5);
 
   Serial.print(year(), DEC);
   Serial.print('/');
@@ -124,5 +163,12 @@ void loop() {
   Serial.print(second(), DEC);
   Serial.println();
 
-  delay(5000);
+  if(a > 11) a=0;
+  Serial.println(a);
+  lightWord(HOURS[a], 120);
+  a++;
+
+  FastLED.show();
+
+  delay(700);
 }
